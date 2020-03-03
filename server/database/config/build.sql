bfd1e089c1a9ccd4e8bc3edf7ffc1429c3a72ebf
Hassan Al-najjar <ha77an19@gmail.com>
2020-03-03 15:35:45 +0200

    created database schema #5

---
+++ server/database/config/build.sql
@@ -0,0 +1,25 @@
+BEGIN;
+
+    DROP TABLE IF EXISTS users,posts
+    CASCADE;
+
+CREATE TABLE users
+(
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password INTEGER NOT NULL,
+    avatar text
+);
+
+CREATE TABLE posts
+(
+    id SERIAL PRIMARY KEY NOT NULL,
+    user_id INTEGER references users(id) NOT NULL,
+    post_title VARCHAR(255) , 
+    post_content text ,
+    post_img text,
+    post_date DATE NOT NULL DEFAULT CURRENT_DATE
+);
+
+COMMIT;
