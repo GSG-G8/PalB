d0bf48a6af482cdbc02c47a369e2ab34ae09cce9
ahmadysalah <a.ahmad.salah@gmail.com>
2020-03-03 15:44:16 +0200

    conection file and build js #7

---
+++ server/database/config/connection.js
@@ -0,0 +1,18 @@
+const { Pool } = require('pg');
+
+require('env2')('config.env');
+
+let dbUrl = '';
+if (process.env.NODE_ENV === 'test') {
+  dbUrl = process.env.TEST_PALB;
+} else if (process.env.NODE_ENV === 'production') {
+  dbUrl = process.env.DATABASE_URL;
+} else {
+  dbUrl = process.env.DEVELOPER_PALB;
+}
+const options = {
+  connectionString: dbUrl,
+  ssl: true,
+};
+
+module.exports = new Pool(options);
